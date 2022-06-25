df02bff1418fbdb31d104b4bedabc104cf142b58
VladDoctor <42406573+VladDoctor@users.noreply.github.com>
2022-06-25 11:05:15 +0300

    main change
    Изменил механизм классификации динозавров

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from GAN_core import GAN_core as who_is
 img = new_image('dino_random') # выгрузка случайной фотографии с динозавром
 species = who_is(img) # определение вида динозавра по фотографии
 
-if species == 'T-rex':
-    print('Selectel')
-else:
-    print(species)
+print('Selectel')
+print(species)
 
 print(dino_weight(img)) #
 
